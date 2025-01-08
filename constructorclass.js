bc5b0461743b3423d55ecdb347957b8f9c94ddd5
Pattharanan <s6406021622052@email.kmutnb.ac.th>
2025-01-08 08:30:59 +0700

    2

---
+++ constructorclass.js
@@ -0,0 +1,9 @@
+class Song {
+    constructor(title, artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+}
+
+const mySong = newSong("Bohemain Rhapssody", "Queen");
+console.log(mySong.title);
